1e58af38e019c749cfb7f9644d594cf44c7e99f0
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2025-08-05 12:10:22 +0530

    Initial commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Siddhatech on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
